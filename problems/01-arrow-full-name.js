d8f5a62d31d63a51c5e6b9da1535ff5f02e2ac84
Vusal Layijov <Layijovvusal@gmail.com>
2022-11-03 17:15:51 -0700

    Second

---
+++ problems/01-arrow-full-name.js
@@ -0,0 +1,36 @@
+/***********************************************************************
+Write a function using fat arrow syntax, `arrowGetFullName(person)` that takes in
+a person object and returns a string containing their full name.
+
+Assign the below function to a variable using the const keyword. Using the const
+keyword will allow any value assigned to that variable protection from being
+reassigned within that scope.
+
+Examples:
+let p1 = {firstName: 'John', lastName: 'Doe'};
+arrowGetFullName(p1); // => 'John Doe'
+let p2 = {firstName: 'Charlie', lastName: 'Brown', age: 9};
+arrowGetFullName(p2); // => 'Charlie Brown'
+
+***********************************************************************/
+
+// Your code here
+
+const arrowGetFullName = (object) => {
+  let fullname = [];
+  for (let key in object) {
+    let keyname = object[key];
+    if (key === 'firstName' || key === 'lastName') {
+    fullname.push(keyname);
+    }
+  }
+  return fullname.join(' ');
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+
+try {
+  module.exports = arrowGetFullName;
+} catch (e) {
+  module.exports = null;
+}
